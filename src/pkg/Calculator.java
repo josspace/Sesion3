69312983df3c947528a17200aa202538b6978ab7
josspace <josspace@josspacem1.home>
2023-10-17 23:20:04 +0200

    TDD 1

---
+++ src/pkg/Calculator.java
@@ -0,0 +1,25 @@
+package pkg;
+
+public class Calculator {
+
+	public static Integer sum(int i, int j) {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	public static Integer subtraction(int i, int j) {
+		// TODO Auto-generated method stub
+		return -1;
+	}
+
+	public static Integer multiplication(int i, int j) {
+		// TODO Auto-generated method stub
+		return 6;
+	}
+
+	public static Integer divide(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/pkg/CalculatorTest.java
@@ -0,0 +1,49 @@
+package pkg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSum() {
+		assertEquals(5, Calculator.sum(2,3));
+	}
+	
+	@Test
+	void testSubtraction() {
+		assertEquals(5, Calculator.subtraction(2,3));
+	}
+	
+	@Test
+	void testMultplication() {
+		assertEquals(5, Calculator.multiplication(2,3));
+	}
+	
+	@Test
+	void testDivide() {
+		assertEquals(5, Calculator.divide(2,3));
+	}
+
+}
